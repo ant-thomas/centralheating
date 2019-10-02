75edd20c4e767c1748e6a62dec9775a3ebadd62f
ant-thomas <anthonythomas@gmail.com>
2019-10-02 21:28:58 +0100

    Add files via upload

---
+++ heating-433-trig-git.sh
@@ -0,0 +1,150 @@
+#!/bin/sh
+#
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+runcheck="`ps cax | grep heating-433 -c`"
+echo $runcheck
+if [ $runcheck -ge 3 ];
+then
+echo "running"
+exit
+else
+:
+fi
+
+
+control="`cat /path/to/control/files/control.txt`"
+room="`cat /path/to/control/files/room.txt`"
+targettemp="`cat /path/to/control/files/targettemp.txt`"
+mintemp=14
+
+echo control - $control
+
+
+#if off - turn off
+if [ "$control" = "off" ];
+then
+echo "Heating Off"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power OFF"
+echo "off" > /path/to/control/files/state.txt
+exit
+else
+:
+fi
+
+#if on - turn on
+if [ "$control" = "on" ];
+then
+echo "Heating On"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power ON"
+echo "on" > /path/to/control/files/state.txt
+exit
+else
+:
+fi
+
+
+#AUTO - not needed without LEDs
+
+if [ "$control" = "auto" ];
+then
+:
+else
+:
+fi
+
+#select which room temp to use and fetch temp from db
+
+if [ "$room" = "bedroom" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from bedroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "living" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from livingroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "box" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from boxroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "front" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from frontroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "kitchen" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from kitchen order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "spare" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from spare order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "attic" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from attic order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "outside" ];
+then
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from outside order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+elif [ "$room" = "autoroom" ];
+then
+currenttempliving=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from livingroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+currenttempbedroom=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from bedroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+currenttemp=$(echo "$currenttempbedroom\n$currenttempliving" | sort -n | tail -1)
+elif [ "$room" = "autoaverage" ];
+then
+currenttempliving=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from livingroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+currenttempbedroom=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from bedroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+currenttempbox=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from boxroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+currenttemp=$(echo $currenttempliving $currenttempbedroom $currenttempbox | awk -v RS=' ' '{sum+=$1; count++} END{print sum/count}')
+else
+currenttemp=$(mysql --host=SERVERIPADDRESS --user=USERNAME --password=PASSWORD temptest -e"select temperature, unixtime from bedroom order by unixtime desc limit 1" --disable-column-names -B | cut -c 1-4)
+fi
+
+echo $room - $currenttemp
+echo target - $targettemp
+echo mintemp - $mintemp
+arpscan="`cat /path/to/control/files/arpscan.txt`"
+boost="`cat /path/to/control/files/boost.txt`"
+echo boost - $boost
+echo arpscan - $arpscan
+#calculate whether current temp is greater than target temp
+tempcalc="`echo $currenttemp'>='$targettemp | bc -l`"
+
+currenttempround="`echo 'scale=0;' $currenttemp'/1' | bc`"
+#echo $currenttempround
+
+
+#if current temp is higher turn off - if current temp is lower turn on
+if [ "$control" = "auto" ] && [ "$boost" = "1" ] && [ "$tempcalc" = "0" ];
+then
+echo "Heating On"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power ON"
+echo "on" > /path/to/control/files/state.txt
+exit
+
+elif [ "$control" = "auto" ] && [ "$currenttempround" -lt "$mintemp" ];
+then
+echo "Heating On"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power ON"
+echo "on" > /path/to/control/files/state.txt
+echo 
+exit
+
+
+elif [ "$control" = "auto" ] && [ "$arpscan" = "1" ];
+then
+echo "Heating Off"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power OFF"
+echo "off" > /path/to/control/files/state.txt
+exit
+elif [ "$control" = "auto" ] && [ "$tempcalc" = "1" ];
+then
+echo "Heating Off"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power OFF"
+echo "off" > /path/to/control/files/state.txt
+exit
+elif [ "$control" = "auto" ] && [ "$tempcalc" = "0" ];
+then
+echo "Heating On"
+curl -s -o /dev/null -G "http://BOILERIPADDRESS/cm" --data-urlencode "cmnd=Power ON"
+echo "on" > /path/to/control/files/state.txt
+exit
+
+fi
+
+#test
